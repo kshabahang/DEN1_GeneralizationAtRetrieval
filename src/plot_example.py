983944aefc5082a15dbe13b6fc22c79feaced30f
Kevin Shabahang <k.shabahang@gmail.com>
2021-04-14 17:31:55 +1000

    time for home

--- src/plot_example.py
+++ src/plot_example.py
@@ -11,111 +11,131 @@ yourknow_vlens=np.load("../rsc/examples/yourknow_vlens.npy")
 vocab = np.load("../rsc/examples/vocab_pruned.npy")
 V = len(vocab)
 
-topK = 10
-activation = []
-slot       = []
-word       = []
-iteration  = []
-frames = 1*youknow
-(n,m) = youknow.shape
-for i in range(n):
-    #slot 1
-    top = list(filter(lambda s : s[0] > 0, sorted(zip(frames[i, :V],vocab))[::-1][1:topK]))
-    if len(top) > 0:
-        top_strengths, top_words = zip(*top)
-        iteration += [i]*len(top)
-        slot += [1]*len(top)
-        activation += list(top_strengths)
-        word += list(top_words)
-
-    #slot 2
-    top = list(filter(lambda s : s[0] > 0, sorted(zip(frames[i, V:],vocab))[::-1][1:topK]))
-    if len(top) > 0:
-        top_strengths, top_words = zip(*top)
-        iteration += [i]*len(top)
-        slot += [2]*len(top)
-        activation += list(top_strengths)
-        word += list(top_words)
 
 
-df = pd.DataFrame({"Activation": activation, "Word":word, "Slot": slot, "Iteration": iteration})
+g_sts1, g_ws1 = zip(*sorted(zip(youknow[-1, :V], vocab))[::-1][:20])
+g_sts2, g_ws2 = zip(*sorted(zip(youknow[-1, V:], vocab))[::-1][:20])
+
+weights_g = []
+for i in range(len(g_ws1)):
+    for j in range(len(g_ws2)):
+        weights_g.append((g_ws1[i], g_ws2[j]))
 
 
-sns.set_theme(style="ticks")
-palette = sns.color_palette("Set2")
-plot = sns.relplot(data = df, x='Iteration', y='Activation', col="Slot", style="Word", kind="line", palette="dark", linewidth=5)
-
-top1 = list(filter(lambda s : s[0] > 0, sorted(zip(frames[-1, :V],vocab))[::-1][1:topK]))
-top2 = list(filter(lambda s : s[0] > 0, sorted(zip(frames[-1, V:],vocab))[::-1][1:topK]))
-
-for i in range(len(top1)):
-    plot.axes[0][0].text(len(frames), top1[i][0], top1[i][1], size=15)
-plot.axes[0][0].axhline(0.0003, color = 'red')
-
-for i in range(len(top2)):
-    plot.axes[0][1].text(len(frames), top2[i][0], top2[i][1], size=15)
-plot.axes[0][1].axhline(0.0003, color = 'red')
 
 
-plt.show()
-#plot.set(ylim=(0,1))
-#plot.set(xlim=(0, min(len(frames_new), len(frames_odd))))
-#plot.set_xticklabels(range(min(len(frames_new), len(frames_odd))))
+ug_sts1, ug_ws1 =  zip(*sorted(zip(yourknow[-1, :V], vocab))[::-1][:20])
+ug_sts2, ug_ws2 =  zip(*sorted(zip(yourknow[-1, V:], vocab))[::-1][:20])
 
-activation = []
-slot       = []
-word       = []
-iteration  = []
-frames = 1*yourknow
-(n,m) = yourknow.shape
-for i in range(n):
-    #slot 1
-    top = list(filter(lambda s : s[0] > 0, sorted(zip(frames[i, :V],vocab))[::-1][1:topK]))
-    if len(top) > 0:
-        top_strengths, top_words = zip(*top)
-        iteration += [i]*len(top)
-        slot += [1]*len(top)
-        activation += list(top_strengths)
-        word += list(top_words)
-
-    #slot 2
-    top = list(filter(lambda s : s[0] > 0, sorted(zip(frames[i, V:],vocab))[::-1][1:topK]))
-    if len(top) > 0:
-        top_strengths, top_words = zip(*top)
-        iteration += [i]*len(top)
-        slot += [2]*len(top)
-        activation += list(top_strengths)
-        word += list(top_words)
-
-
-df = pd.DataFrame({"Activation": activation, "Word":word, "Slot": slot, "Iteration": iteration})
-
-
-sns.set_theme(style="ticks")
-palette = sns.color_palette("Set2")
-plot = sns.relplot(data = df, x='Iteration', y='Activation', col="Slot", style="Word", kind="line", palette="dark", linewidth=5)
-
-top1 = list(filter(lambda s : s[0] > 0, sorted(zip(frames[-1, :V],vocab))[::-1][1:topK]))
-top2 = list(filter(lambda s : s[0] > 0, sorted(zip(frames[-1, V:],vocab))[::-1][1:topK]))
-
-for i in range(len(top1)):
-    plot.axes[0][0].text(len(frames), top1[i][0], top1[i][1], size=15)
-plot.axes[0][0].axhline(0.0003, color = 'red')
-
-for i in range(len(top2)):
-    plot.axes[0][1].text(len(frames), top2[i][0], top2[i][1], size=15)
-plot.axes[0][1].axhline(0.0003, color = 'red')
-
-plt.show()
+weights_ug = []
+for i in range(len(ug_ws1)):
+    for j in range(len(ug_ws2)):
+        weights_ug.append((ug_ws1[i], ug_ws2[j]))
+#topK = 10
+#activation = []
+#slot       = []
+#word       = []
+#iteration  = []
+#frames = 1*youknow
+#(n,m) = youknow.shape
+#for i in range(n):
+#    #slot 1
+#    top = list(filter(lambda s : s[0] > 0, sorted(zip(frames[i, :V],vocab))[::-1][1:topK]))
+#    if len(top) > 0:
+#        top_strengths, top_words = zip(*top)
+#        iteration += [i]*len(top)
+#        slot += [1]*len(top)
+#        activation += list(top_strengths)
+#        word += list(top_words)
+#
+#    #slot 2
+#    top = list(filter(lambda s : s[0] > 0, sorted(zip(frames[i, V:],vocab))[::-1][1:topK]))
+#    if len(top) > 0:
+#        top_strengths, top_words = zip(*top)
+#        iteration += [i]*len(top)
+#        slot += [2]*len(top)
+#        activation += list(top_strengths)
+#        word += list(top_words)
 
 
-vlens = np.hstack([youknow_vlens[:, 0], yourknow_vlens[:, 0]])
-iteration = list(range(len(youknow_vlens))) + list(range(len(yourknow_vlens)))
-probe = ["you know"]*len(youknow_vlens) + ["your know"]*len(yourknow_vlens)
-df = pd.DataFrame({"Familiarity": vlens, "Probe":probe, "Iteration":iteration})
-
-plot = sns.relplot(data = df, x='Iteration', y='Familiarity', hue= "Probe", style="Probe", kind="line", palette="dark", linewidth=5)
-plt.show()
+#df = pd.DataFrame({"Activation": activation, "Word":word, "Slot": slot, "Iteration": iteration})
+#
+#
+#sns.set_theme(style="ticks")
+#palette = sns.color_palette("Set2")
+#plot = sns.relplot(data = df, x='Iteration', y='Activation', col="Slot", style="Word", kind="line", palette="dark", linewidth=5)
+#
+#top1 = list(filter(lambda s : s[0] > 0, sorted(zip(frames[-1, :V],vocab))[::-1][1:topK]))
+#top2 = list(filter(lambda s : s[0] > 0, sorted(zip(frames[-1, V:],vocab))[::-1][1:topK]))
+#
+#for i in range(len(top1)):
+#    plot.axes[0][0].text(len(frames), top1[i][0], top1[i][1], size=15)
+#plot.axes[0][0].axhline(0.0003, color = 'red')
+#
+#for i in range(len(top2)):
+#    plot.axes[0][1].text(len(frames), top2[i][0], top2[i][1], size=15)
+#plot.axes[0][1].axhline(0.0003, color = 'red')
+#
+#
+#plt.show()
+##plot.set(ylim=(0,1))
+##plot.set(xlim=(0, min(len(frames_new), len(frames_odd))))
+##plot.set_xticklabels(range(min(len(frames_new), len(frames_odd))))
+#
+#activation = []
+#slot       = []
+#word       = []
+#iteration  = []
+#frames = 1*yourknow
+#(n,m) = yourknow.shape
+#for i in range(n):
+#    #slot 1
+#    top = list(filter(lambda s : s[0] > 0, sorted(zip(frames[i, :V],vocab))[::-1][1:topK]))
+#    if len(top) > 0:
+#        top_strengths, top_words = zip(*top)
+#        iteration += [i]*len(top)
+#        slot += [1]*len(top)
+#        activation += list(top_strengths)
+#        word += list(top_words)
+#
+#    #slot 2
+#    top = list(filter(lambda s : s[0] > 0, sorted(zip(frames[i, V:],vocab))[::-1][1:topK]))
+#    if len(top) > 0:
+#        top_strengths, top_words = zip(*top)
+#        iteration += [i]*len(top)
+#        slot += [2]*len(top)
+#        activation += list(top_strengths)
+#        word += list(top_words)
+#
+#
+#df = pd.DataFrame({"Activation": activation, "Word":word, "Slot": slot, "Iteration": iteration})
+#
+#
+#sns.set_theme(style="ticks")
+#palette = sns.color_palette("Set2")
+#plot = sns.relplot(data = df, x='Iteration', y='Activation', col="Slot", style="Word", kind="line", palette="dark", linewidth=5)
+#
+#top1 = list(filter(lambda s : s[0] > 0, sorted(zip(frames[-1, :V],vocab))[::-1][1:topK]))
+#top2 = list(filter(lambda s : s[0] > 0, sorted(zip(frames[-1, V:],vocab))[::-1][1:topK]))
+#
+#for i in range(len(top1)):
+#    plot.axes[0][0].text(len(frames), top1[i][0], top1[i][1], size=15)
+#plot.axes[0][0].axhline(0.0003, color = 'red')
+#
+#for i in range(len(top2)):
+#    plot.axes[0][1].text(len(frames), top2[i][0], top2[i][1], size=15)
+#plot.axes[0][1].axhline(0.0003, color = 'red')
+#
+#plt.show()
+#
+#
+#vlens = np.hstack([youknow_vlens[:, 0], yourknow_vlens[:, 0]])
+#iteration = list(range(len(youknow_vlens))) + list(range(len(yourknow_vlens)))
+#probe = ["you know"]*len(youknow_vlens) + ["your know"]*len(yourknow_vlens)
+#df = pd.DataFrame({"Familiarity": vlens, "Probe":probe, "Iteration":iteration})
+#
+#plot = sns.relplot(data = df, x='Iteration', y='Familiarity', hue= "Probe", style="Probe", kind="line", palette="dark", linewidth=5)
+#plt.show()
 
 
 
--- src/plot_graph.py
+++ src/plot_graph.py
@@ -4,36 +4,48 @@ import pickle
 import sys
 
 
-corr_file = "top_weightsNNS_VBP_2_NN_VBP_children_learn_corr.pkl"
-incorr_file = "top_weightsNNS_VBP_2_NN_VBP_child_learn_incorr.pkl"
+#corr_file = "top_weightsNNS_VBP_2_NN_VBP_children_learn_corr.pkl"
+#incorr_file = "top_weightsNNS_VBP_2_NN_VBP_child_learn_incorr.pkl"
+#
+#cond = sys.argv[1]
+#if cond == "corr":
+#    f = open(corr_file, "rb")
+#    top_weights = pickle.load(f)
+#    f.close()
+#else:
+#    f = open(incorr_file, "rb")
+#    top_weights = pickle.load(f)
+#    f.close()
+#n = int(sys.argv[2])
+#k = int(sys.argv[3])
+#(ws1, ws2, sts1, sts2, weights, vlens) = top_weights[n]
+#
+#vlens = vlens[n] #TODO fix this so we're not sending clones of vlen
+#print("{} --- Vlen: {}  Vlen1: {}  Vlen2: {}".format(cond, round(vlens[0],4), round(vlens[1], 4), round(vlens[2], 4)))
+#
+#G = nx.Graph()
+##G.add_nodes_from(ws1[:k] + ws2[:k])
+#for i in range(k):
+#    for j in range(k):
+#        if weights[i,j] != 0:
+#            G.add_edge( ws1[i], " "+ws2[j], weight=10*weights[i,j])
+#
+#edges = G.edges()
+#weight= [G[u][v]['weight'] for u,v in edges]
+#
+#pos = nx.bipartite_layout(G, ws1) 
+#fig = plt.figure()
+#nx.draw(G, pos = pos, node_color="white", with_labels=True, alpha=0.5, width=weight)
+#fig.show()
 
-cond = sys.argv[1]
-if cond == "corr":
-    f = open(corr_file, "rb")
-    top_weights = pickle.load(f)
-    f.close()
-else:
-    f = open(incorr_file, "rb")
-    top_weights = pickle.load(f)
-    f.close()
-n = int(sys.argv[2])
-k = int(sys.argv[3])
-(ws1, ws2, sts1, sts2, weights, vlens) = top_weights[n]
-
-vlens = vlens[n] #TODO fix this so we're not sending clones of vlen
-print("{} --- Vlen: {}  Vlen1: {}  Vlen2: {}".format(cond, round(vlens[0],4), round(vlens[1], 4), round(vlens[2], 4)))
+g1= np.load("../rsc/examples/g1.npy")
+g2=np.load("../rsc/examples/g2.npy")
+ug1=np.load("../rsc/examples/ug1.npy")
+ug2=np.load("../rsc/examples/ug2.npy")
+Wg = np.load("../rsc/examples/Wg.npy")
+Wug=np.load("../rsc/examples/Wug.npy")
 
 G = nx.Graph()
-#G.add_nodes_from(ws1[:k] + ws2[:k])
-for i in range(k):
-    for j in range(k):
-        if weights[i,j] != 0:
-            G.add_edge( ws1[i], " "+ws2[j], weight=10*weights[i,j])
 
-edges = G.edges()
-weight= [G[u][v]['weight'] for u,v in edges]
-
-pos = nx.bipartite_layout(G, ws1) 
-fig = plt.figure()
-nx.draw(G, pos = pos, node_color="white", with_labels=True, alpha=0.5, width=weight)
-fig.show()
+g1 = list(g1[0][:20])
+g2 = list(g2[0][:20])
