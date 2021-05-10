ec1c54350d3540f95c9fc4e5f6ae6578047f8bcc
Kevin Shabahang <k.shabahang@gmail.com>
2021-05-10 14:53:31 +1000

    yea

--- src/toy.py
+++ src/toy.py
@@ -21,8 +21,8 @@ vectors = [[1,1,-1,-1], [-1,1,-1,1], [1,1,1,1], [1,-1,-1,1]]
 #wvecs = {"the":[1,1,-1,-1], "cat":[-1,1,-1,1], "a":[1,1,1,1], "dog":[1,-1,-1,1]}
 vocab = ['the','cat', 'a', 'dog']
 
-NIterProbe = 1
-NIterEncode= 1
+NIterProbe = 500
+NIterEncode=1
 
 
 resps = {}
@@ -30,14 +30,17 @@ resps = {}
 
 for m in range(NIterEncode):
     word_assignment = list(range(len(vocab)))
-    #np.random.shuffle(word_assignment)
+    np.random.shuffle(word_assignment)
     
     wvecs = {vocab[word_assignment[i]]:vectors[i] for i in range(len(vocab))}
+    for j in range(len(vocab)):
+        print(vocab[j], wvecs[vocab[j]])
     
     #wvecs = {w:wvecs[w]/np.linalg.norm(wvecs[w]) for w in wvecs.keys()}
     
     wvec_mat = np.vstack([wvecs['the'], wvecs['cat'], wvecs['a'], wvecs['dog']])
-    
+   
+
     
     p1 =  np.hstack([wvecs['the'], wvecs['cat']])
     p2 =  np.hstack([wvecs['a'], wvecs['dog']])
@@ -55,8 +58,13 @@ for m in range(NIterEncode):
                 "odd1":np.hstack([wvecs['dog'], wvecs['the']]),
                 "odd2":np.hstack([wvecs['cat'], wvecs['a']])}
     
-    
-    runDEN = False
+    ei, ev = np.linalg.eig(W) 
+
+    isort = np.argsort(ei)[::-1][:2]
+    print(ei[isort])
+    print(ev[:, isort])
+
+    runDEN = True
     runBSB = False
     
     noise = 0.3
@@ -67,6 +75,7 @@ for m in range(NIterEncode):
         for probe_type in probes.keys():
             resps = []
             for l in range(NIterProbe):
+                loadings = []
                 probe = probes[probe_type] + np.random.normal(0, noise)
                 probe /= np.linalg.norm(probe)
                 
@@ -74,13 +83,14 @@ for m in range(NIterEncode):
                 W_prime = W + np.outer(x, x)
                 x_prime = probe.dot(W_prime)
                 x_prime /= np.linalg.norm(x_prime)
+                loadings.append(x_prime.dot(ev[isort].T))
                 k = 1
                 while(np.linalg.norm(x - x_prime) > 1e-7):
                     x = 1*x_prime
                     x_prime = x.dot(W_prime)
                     x_prime /= np.linalg.norm(x_prime)
                     k += 1
-                
+                    loadings.append(x_prime.dot(ev[isort].T))               
                 
                 s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
                 s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
@@ -137,148 +147,148 @@ for m in range(NIterEncode):
                 print(cue, counts[cue]/NIterProbe)
 
 
-
-
-###DEN plots
-p_fine  = probes['new1']
-p_odd   = probes['odd1']
-fam_new = []
-frames = {"the dog":[], "dog the":[]}
-
-probe = p_fine + np.random.normal(0, noise)
-probe /= np.linalg.norm(probe)
-x = 1*probe
-W_prime = W + np.outer(x, x)
-x_prime = probe.dot(W_prime)
-fam_new.append(np.linalg.norm(x_prime))
-x_prime /= np.linalg.norm(x_prime)
-
-s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
-s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
-
-frames["the dog"].append(np.hstack([s1,s2]))
-
-
-k = 1
-while(np.linalg.norm(x - x_prime) > 1e-7):
-    x = 1*x_prime
-    x_prime = x.dot(W_prime)
+toPlot = False
+if toPlot:
+    ###DEN plots
+    p_fine  = probes['new1']
+    p_odd   = probes['odd1']
+    fam_new = []
+    frames = {"the dog":[], "dog the":[]}
+    
+    probe = p_fine + np.random.normal(0, noise)
+    probe /= np.linalg.norm(probe)
+    x = 1*probe
+    W_prime = W + np.outer(x, x)
+    x_prime = probe.dot(W_prime)
     fam_new.append(np.linalg.norm(x_prime))
     x_prime /= np.linalg.norm(x_prime)
-
+    
     s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
     s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
-
-
-
+    
     frames["the dog"].append(np.hstack([s1,s2]))
-    k += 1
-
-                                                                                   
-s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
-s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
-                                                                                   
-                                                                                   
-w1 = vocab[np.argmax(s1)]
-w2 = vocab[np.argmax(s2)]
-
-resp = w1 + " " + w2
-
-
-
-frames_new = np.array(frames['the dog'])
-fam_odd = []
-probe = p_odd + np.random.normal(0, noise)
-probe /= np.linalg.norm(probe)
-x = 1*probe
-W_prime = W + np.outer(x, x)
-x_prime = probe.dot(W_prime)
-fam_odd.append(np.linalg.norm(x_prime))
-x_prime /= np.linalg.norm(x_prime)
-s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
-s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
-
-frames["dog the"].append(np.hstack([s1,s2]))
-
-k = 1
-while(np.linalg.norm(x - x_prime) > 1e-7):
-    x = 1*x_prime
-    x_prime = x.dot(W_prime)
+    
+    
+    k = 1
+    while(np.linalg.norm(x - x_prime) > 1e-7):
+        x = 1*x_prime
+        x_prime = x.dot(W_prime)
+        fam_new.append(np.linalg.norm(x_prime))
+        x_prime /= np.linalg.norm(x_prime)
+    
+        s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
+        s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
+    
+    
+    
+        frames["the dog"].append(np.hstack([s1,s2]))
+        k += 1
+    
+                                                                                       
+    s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
+    s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
+                                                                                       
+                                                                                       
+    w1 = vocab[np.argmax(s1)]
+    w2 = vocab[np.argmax(s2)]
+    
+    resp = w1 + " " + w2
+    
+    
+    
+    frames_new = np.array(frames['the dog'])
+    fam_odd = []
+    probe = p_odd + np.random.normal(0, noise)
+    probe /= np.linalg.norm(probe)
+    x = 1*probe
+    W_prime = W + np.outer(x, x)
+    x_prime = probe.dot(W_prime)
     fam_odd.append(np.linalg.norm(x_prime))
     x_prime /= np.linalg.norm(x_prime)
-
-
     s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
     s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
-
-
-
+    
     frames["dog the"].append(np.hstack([s1,s2]))
-    k += 1
-
-                                                                                   
-s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
-s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
-                                                                                   
-                                                                                   
-w1 = vocab[np.argmax(s1)]
-w2 = vocab[np.argmax(s2)]
-
-resp = w1 + " " + w2
-
-frames_new = np.array(frames['the dog'])
-frames_odd = np.array(frames['dog the'])
-
-
-
-activation = []
-slot = []
-word = []
-iteration=[]
-(n, m) = frames_new.shape 
-for i in range(m):
-    activation +=  list( frames_new[:, i] )
-    if i < 4:
-        slot += [1]*n
-    else:
-        slot += [2]*n
-    iteration += range(n)
-    word += [vocab[i%4]]*n
-
-df = pd.DataFrame({"Activation": activation, "Word":word, "Slot": slot, "Iteration": iteration})
-
-sns.set_theme(style="ticks")
-palette = sns.color_palette("Set2")
-plot = sns.relplot(data = df, x='Iteration', y='Activation', col="Slot", style="Word", kind="line", hue="Word", palette="dark", linewidth=8)
-plot.set(ylim=(0,1))
-plot.set(xlim=(0, min(len(frames_new), len(frames_odd))))
-plot.set_xticklabels(range(min(len(frames_new), len(frames_odd))))
-
-
-
-
-
-activation = []
-slot = []
-word = []
-iteration=[]
-(n, m) = frames_odd.shape 
-for i in range(m):
-    activation +=  list( frames_odd[:, i] )
-    if i < 4:
-        slot += [1]*n
-    else:
-        slot += [2]*n
-    iteration += range(n)
-    word += [vocab[i%4]]*n
-
-df = pd.DataFrame({"Activation": activation, "Word":word, "Slot": slot, "Iteration": iteration})
-
-
-palette = sns.color_palette("plasma")
-sns.set_theme(style="ticks")
-plot = sns.relplot(data = df, x='Iteration', y='Activation', col="Slot", style="Word", kind="line", hue="Word", linewidth=8)
-plot.set(ylim=(0,1))
-plot.set(xlim=(0, min(len(frames_new), len(frames_odd))))
-plot.set_xticklabels(range(min(len(frames_new), len(frames_odd))))
-plt.show()
+    
+    k = 1
+    while(np.linalg.norm(x - x_prime) > 1e-7):
+        x = 1*x_prime
+        x_prime = x.dot(W_prime)
+        fam_odd.append(np.linalg.norm(x_prime))
+        x_prime /= np.linalg.norm(x_prime)
+    
+    
+        s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
+        s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
+    
+    
+    
+        frames["dog the"].append(np.hstack([s1,s2]))
+        k += 1
+    
+                                                                                       
+    s1 = np.array([np.abs(vcos(x_prime[:4], wvec_mat[j])) for j in range(len(vocab))])
+    s2 = np.array([np.abs(vcos(x_prime[4:], wvec_mat[j])) for j in range(len(vocab))])
+                                                                                       
+                                                                                       
+    w1 = vocab[np.argmax(s1)]
+    w2 = vocab[np.argmax(s2)]
+    
+    resp = w1 + " " + w2
+    
+    frames_new = np.array(frames['the dog'])
+    frames_odd = np.array(frames['dog the'])
+    
+    
+    
+    activation = []
+    slot = []
+    word = []
+    iteration=[]
+    (n, m) = frames_new.shape 
+    for i in range(m):
+        activation +=  list( frames_new[:, i] )
+        if i < 4:
+            slot += [1]*n
+        else:
+            slot += [2]*n
+        iteration += range(n)
+        word += [vocab[i%4]]*n
+    
+    df = pd.DataFrame({"Activation": activation, "Word":word, "Slot": slot, "Iteration": iteration})
+    
+    sns.set_theme(style="ticks")
+    palette = sns.color_palette("Set2")
+    plot = sns.relplot(data = df, x='Iteration', y='Activation', col="Slot", style="Word", kind="line", hue="Word", palette="dark", linewidth=8)
+    plot.set(ylim=(0,1))
+    plot.set(xlim=(0, min(len(frames_new), len(frames_odd))))
+    plot.set_xticklabels(range(min(len(frames_new), len(frames_odd))))
+    
+    
+    
+    
+    
+    activation = []
+    slot = []
+    word = []
+    iteration=[]
+    (n, m) = frames_odd.shape 
+    for i in range(m):
+        activation +=  list( frames_odd[:, i] )
+        if i < 4:
+            slot += [1]*n
+        else:
+            slot += [2]*n
+        iteration += range(n)
+        word += [vocab[i%4]]*n
+    
+    df = pd.DataFrame({"Activation": activation, "Word":word, "Slot": slot, "Iteration": iteration})
+    
+    
+    palette = sns.color_palette("plasma")
+    sns.set_theme(style="ticks")
+    plot = sns.relplot(data = df, x='Iteration', y='Activation', col="Slot", style="Word", kind="line", hue="Word", linewidth=8)
+    plot.set(ylim=(0,1))
+    plot.set(xlim=(0, min(len(frames_new), len(frames_odd))))
+    plot.set_xticklabels(range(min(len(frames_new), len(frames_odd))))
+    plt.show()
