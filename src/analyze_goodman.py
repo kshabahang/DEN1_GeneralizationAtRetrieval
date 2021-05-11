755f1deb38ce81f3aeba8313e9fcba41c49d7d20
Kevin Shabahang <k.shabahang@gmail.com>
2021-05-11 14:51:33 +1000

    not good enough to report

--- src/analyze_goodman.py
+++ src/analyze_goodman.py
@@ -1,10 +1,13 @@
 from matplotlib import pyplot as plt
 import pickle
 import numpy as np
-from scipy.stats import ttest_rel
+from scipy.stats import ttest_reli, ttest_ind
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+import spacy
+
+nlp =  spacy.load("en_core_web_lg")
 
 
 conds = ['intact','lesioned']
@@ -22,6 +25,55 @@ if True:
     
     goodman_corr = np.array([goodman['correct']['vlens'][i][-1][0] for i in range(len(goodman['correct']['vlens']))])
     goodman_incr = np.array([goodman['incorrect']['vlens'][i][-1][0] for i in range(len(goodman['incorrect']['vlens']))])
+
+
+
+    vlens_verbs = []
+    vlens_nouns = []
+    for i in range(len(goodman['correct']['probe'])):
+        bg = goodman['correct']['probe'][i]
+        [w1,w2] = bg.split()
+        pos1 = nlp(w1)[0].pos_
+        pos2 = nlp(w2)[0].pos_
+        if pos2 == "NOUN":
+            vlens_nouns.append( goodman['correct']['vlens'][i][-1][0] )
+        elif pos2 == "VERB":
+            vlens_verbs.append( goodman['correct']['vlens'][i][-1][0] )
+
+    ttest = ttest_ind(vlens_nouns, vlens_verbs)
+    t = np.round(ttest[0],2)
+    p = np.round(ttest[1],3)
+    M1 = np.round(np.mean(vlens_nouns),5)
+    M2 = np.round(np.mean(vlens_verbs),5)
+    df= min([len(vlens_nouns)-1, len(vlens_verbs)-1])
+    print("Mean(Noun): {} --- Mean(Verb): {} --- t({}) = {}, p = {}".format(M1, M2, df, t, p ))
+
+
+
+
+
+    vlens_verbs = []
+    vlens_nouns = []
+    for i in range(len(goodman['incorrect']['probe'])):
+        bg = goodman['incorrect']['probe'][i]
+        [w1,w2] = bg.split()
+        pos1 = nlp(w1)[0].pos_
+        pos2 = nlp(w2)[0].pos_
+        if pos2 == "NOUN":
+            vlens_nouns.append( goodman['incorrect']['vlens'][i][-1][0] )
+        elif pos2 == "VERB":
+            vlens_verbs.append( goodman['incorrect']['vlens'][i][-1][0] )
+
+
+    ttest = ttest_ind(vlens_nouns, vlens_verbs)
+    t = np.round(ttest[0],2)
+    p = np.round(ttest[1],3)
+    M1 = np.round(np.mean(vlens_nouns),5)
+    M2 = np.round(np.mean(vlens_verbs),5)
+    df= min([len(vlens_nouns)-1, len(vlens_verbs)-1])
+    print("Mean(Noun): {} --- Mean(Verb): {} --- t({}) = {}, p = {}".format(M1, M2, df, t, p ))
+
+
 
     diff = goodman_corr - goodman_incr
 
