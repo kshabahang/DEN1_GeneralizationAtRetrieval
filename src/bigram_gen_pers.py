45dde70fa01888e3f6802c6e055d262e4713f32a
Ubuntu <ubuntu@tulving.novalocal>
2021-08-04 09:48:05 +0000

    alright

--- src/bigram_gen_pers.py
+++ src/bigram_gen_pers.py
@@ -223,7 +223,7 @@ if __name__ == "__main__":
 
 
     
-    toLesion = True
+    toLesion = False
 
     pairs = "VB_RBR_2_RBR_VB PPRS_NN_2_PPR_NN IN_VBG_2_IN_VBP NNS_VBP_2_NN_VBP NN_VBZ_2_NN_VBP DT_NN_2_NN_DT JJ_NN_2_NN_JJ NN_IN_2_IN_NN PPR_VBP_2_PPRS_VBP".split()#[1:]
 
